cc51655f027943e70efa91e841edae1b448e8019
Levon Gabrielyan <levongabrielyan44@gmail.com>
2023-07-11 11:13:17 +0400

    Task 3 added

--- Day_4_Exeption_Handling/Program.cs
+++ Day_4_Exeption_Handling/Program.cs
@@ -1,4 +1,7 @@
-﻿class Tasks
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+class Tasks
 {
     public static void SaveInOtherFile(string A,string B)//Task 1
     {
@@ -41,6 +44,66 @@
     }
 
 }
+class Subscriber //Task 4
+{
+    string phoneNumber;
+    int balance ;
+    bool isInRoaming;
+    bool serviceIsActive;
+    DateTime expirationDate;
+    public Subscriber(string phoneNumber, int balance, bool isInRoaming,bool sa, DateTime expirationDate)
+    {
+        this.phoneNumber = phoneNumber;
+        this.balance = balance;
+        this.isInRoaming = isInRoaming;
+        this.expirationDate = expirationDate;
+        this.serviceIsActive = sa;
+    }
+    public bool activateService()
+    {
+        try
+        {
+            if (isInRoaming)
+                throw new RoamingException();
+            else if(!((expirationDate - DateTime.Now).TotalDays >= 10))
+                throw new InvalidExpirationDate();
+            else if(serviceIsActive)
+                throw new ServiceAlreadyActiveException();
+            else if(!(balance >= 1500))
+                throw new InsufficientBalanceException();
+            serviceIsActive = true;
+            balance -= 1500;
+            Console.WriteLine("Service activated");
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return false;
+        }
+        
+    }
+}
+class InvalidExpirationDate : Exception
+{
+    public InvalidExpirationDate() : base("Expiration date shold be at least 10 days ahead of date now") { }
+    public InvalidExpirationDate(string m): base(m) { }
+}
+class RoamingException : Exception
+{
+    public RoamingException() : base("Roaming shold be off") { }
+    public RoamingException(string m): base(m) { }
+}
+class ServiceAlreadyActiveException : Exception
+{
+    public ServiceAlreadyActiveException() : base("Service is already activated") { }
+    public ServiceAlreadyActiveException(string m): base(m) { }
+}
+class InsufficientBalanceException : Exception
+{
+    public InsufficientBalanceException() : base("Not enough money in balance") { }
+    public InsufficientBalanceException(string m): base(m) { }
+}
 class Program
 {
     static void Main()
@@ -52,6 +115,7 @@ class Program
         //Console.WriteLine(Tasks.ReadAge());
 
         //Task 3
-
+        var subscriber = new Subscriber("096420414",10000,false,false,DateTime.Now);
+        subscriber.activateService();
     }
 }
